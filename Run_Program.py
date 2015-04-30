eef69415e5922fdd1f1b19fc32cd369418282c01
wiwatowasirikul <wiwat.owa@mahidol.ac.th>
2015-04-30 11:16:26 +0700

    Add Run_Program.py

--- Run_Program.py
+++ Run_Program.py
@@ -19,10 +19,9 @@ def ModuleMain():
         Model_index = sys.argv[5]   #User defined PCM modeling 
         CV_Method = sys.argv[6]     #k-folds, LOO
         FeatureSelectionMode = sys.argv[7] #None,VIP
-        SpiltMethod = sys.argv[8]          #Random, PCA, KMean, HC
-        SpiltCriteria = float(sys.argv[9]) # 0.15 by default
-        Iteration = int(sys.argv[10])       #20  by default
-        NumPermute = int(sys.argv[11])      # 100  by default
+        SpiltCriteria = float(sys.argv[8]) # 0.15 by default
+        Iteration = int(sys.argv[9])       #20  by default
+        NumPermute = int(sys.argv[10])      # 100  by default
     
     
     import Descriptors_Extraction as Ex
@@ -30,9 +29,8 @@ def ModuleMain():
 
     userdefined = Ex.UserDefined(Rawfile,Indicator,Ligand_index,Protein_index, Model_index,
                              SpiltCriteria,CV_Method,FeatureSelectionMode, Iteration, 
-                             NumPermute, SpiltMethod)
-    DL,DP,Y, userdefined = Ex.AnalysisInputfile(userdefined)
-    pcm.Xval(DL,DP,Y, userdefined)
+                             NumPermute)
+    Ex.AnalysisInputfile(userdefined)
     X, Y, H, harray, NumDes = pcm.Model_Selection(userdefined)
     ind_ext = pcm.Index_Train_Ext(X, userdefined)
     Mean, SD, YpTr, YpCv, YpExt = pcm.Prediction(X,Y,ind_ext, userdefined)
